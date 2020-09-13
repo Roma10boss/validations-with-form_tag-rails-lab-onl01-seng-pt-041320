ecf8a67f43b117558bf043aafaf049eccce7bbe1
Romario Gustave <romagustave10@gmail.com>
2020-09-13 17:17:53 +0000

    Automatically backed up by Learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,15 @@
 class Author < ActiveRecord::Base
+  validates :name, format: { without: /[0-9]/, message: "does not allow numbers" }
+  validates :email, uniqueness: true
+
+  def create 
+    @author = Author.new(author_name)
+
+      if @author.valid?
+        @author.save
+        redirect_to_author_path(@author)
+      else 
+        render :new
+      end 
+    end 
 end
